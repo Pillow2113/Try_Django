85eb316264a00a1e5c6e79f008ae7febe410167e
Pillow2113 <90503696+Pillow2113@users.noreply.github.com>
2024-04-19 10:15:45 +0800

    Add files via upload

---
+++ Django/mysite/polls/views.py
@@ -0,0 +1,14 @@
+
+from .models import Question
+ 
+from django.shortcuts import get_object_or_404, render
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
